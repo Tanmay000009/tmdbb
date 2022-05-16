b863e1c2bae0fa3106a1d6de8d3b75a4814caa5c
Tanmay000009 <tanmayvyas09@gmail.com>
2022-05-16 15:14:54 +0530

    update ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,41 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 // react search
 function App() {
-  const [APIData, setAPIData] = useState([]);
+  const [movies, setMovies] = useState([
+    {
+      _id: "6280b7f1290dd91c7c1c1c23",
+      title: "Doctor Strange",
+      imgLink: "abcdefg",
+      __v: 0,
+    },
+  ]);
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/movies/`, {
-        "Content-Type": "application/json",
-      })
-      .then((response) => {
-        setAPIData(response.data).error((error) => {
-          console.log(error);
+    async function fetchData() {
+      await axios
+        .get(`http://localhost:5000/movies/`, {
+          "Content-Type": "application/json",
+        })
+        .then((response) => {
+          console.log(response.data);
+          var data = response.data;
+          setMovies(data);
+          console.log(movies);
+          console.log(data[0]);
         });
-        console.log(response.data);
-      });
+    }
+    fetchData();
   }, []);
   return (
-    <div class="flex justify-center">
-      <div class="w-full">
+    <div className="flex justify-center">
+      {movies.map((movie) => (
+        <div key={movie._id}>{movie.name}</div>
+      ))}
+      <div className="w-full">
         <input
           type="text"
           name="name"
           placeholder="Search..."
-          class="w-1/3 ml-96 py-2 border-b-2 border-green-600 outline-none focus:border-blue-400"
+          className="w-1/3 ml-96 py-2 border-b-2 border-green-600 outline-none focus:border-blue-400"
         />
       </div>
     </div>
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ db();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use(express.json());
 app.use(express.urlencoded());
 dotenv.config({ path: "config.env" });

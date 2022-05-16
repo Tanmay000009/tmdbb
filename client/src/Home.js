ae8316c8c7fee75c86a3b18be7ef929c48b449ef
Tanmay000009 <tanmayvyas09@gmail.com>
2022-05-16 19:20:41 +0530

    connect routes

--- client/src/Home.js
+++ client/src/Home.js
@@ -70,11 +70,7 @@ function Home() {
         {movies.map((movie) => (
           <div key={movie._id} className="">
             <div className=" max-w-sm rounded overflow-hidden shadow-lg">
-              <img
-                className="w-full"
-                src="/img/card-top.jpg"
-                alt="Movie Poster"
-              />
+              <img className="w-full" src={movie.imgLink} alt="Movie Poster" />
               <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{movie.title}</div>
               </div>
--- client/src/movieForm.jsx
+++ client/src/movieForm.jsx
@@ -2,6 +2,37 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function MovieForm() {
+  const [movieName, setMovieName] = useState("");
+  const [imgLink, imgLinkName] = useState("");
+
+  const updateValue = (e) => {
+    if (e.target.name === "movieName") {
+      setMovieName(e.target.value);
+    } else if (e.target.name === "imgLink") {
+      imgLinkName(e.target.value);
+    }
+  };
+
+  const submitMovie = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/movies/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: movieName,
+        imgLink: imgLink,
+      }),
+    });
+    if (response.status === 200) {
+      window.location.href = "/";
+    } else {
+      console.log(response);
+      alert("Something went wrong");
+    }
+  };
+
   return (
     <div className="bg-gray-800 min-h-screen">
       <div className="pl-8 text-xl mb-16">
@@ -22,24 +53,26 @@ function MovieForm() {
         </ul>
       </div>
       <div className="grid place-items-center mt-48">
-        <div className="p-4 max-w-sm bg-white rounded-lg  shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6" action="#">
+        <form className="space-y-6" action="#">
+          <div className="p-4 max-w-sm bg-white rounded-lg  shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
             <h5 className="text-xl font-medium text-gray-900 dark:text-white">
               Add movie
             </h5>
             <div>
               <label
-                for="name"
+                for="movieName"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Name
               </label>
               <input
                 type="text"
-                name="name"
-                id="name"
+                name="movieName"
+                id="movieName"
+                value={movieName}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 placeholder="Movie name"
+                onChange={updateValue}
                 required
               />
             </div>
@@ -54,13 +87,22 @@ function MovieForm() {
                 type="text"
                 name="imgLink"
                 id="imgLink"
+                value={imgLink}
+                onChange={updateValue}
                 placeholder="Banner Link"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 required
               />
             </div>
-          </form>
-        </div>
+            <button
+              type="button"
+              className="mt-4 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+              onClick={submitMovie}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
